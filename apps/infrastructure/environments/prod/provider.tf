bd3c193ca9eda3b2d5f60c52a80b26dbf202190c
Bryce Klinker <bryce.klinker@gmail.com>
2021-06-20 07:32:06 -0500

    Specifying providers

--- apps/infrastructure/environments/prod/provider.tf
+++ apps/infrastructure/environments/prod/provider.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hasicorp/azurerm"
+      version = "=2.64.0"
+    }
+
+    auth0 = {
+      source = "alexkapa/auth0"
+      version = "=0.21.0"
+    }
+  }
+}
+
+
 provider "azurerm" {
   features {}
 }
