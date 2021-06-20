cb2c35f1f4c69f06c201b75ab19eb2c84e26062a
Bryce Klinker <bryce.klinker@gmail.com>
2021-06-20 07:38:33 -0500

    Attempting to fix version issues

--- apps/infrastructure/environments/prod/provider.tf
+++ apps/infrastructure/environments/prod/provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source = "hasicorp/azurerm"
-      version = "=2.64.0"
+      version = ">= 2.64.0"
     }
 
     auth0 = {
       source = "alexkapa/auth0"
-      version = "=0.21.0"
+      version = ">= 0.21.0"
     }
   }
 
--- apps/infrastructure/modules/azure_static_site/provider.tf
+++ apps/infrastructure/modules/azure_static_site/provider.tf
@@ -2,12 +2,20 @@ terraform {
   required_providers {
     azurerm = {
       source = "hasicorp/azurerm"
-      version = "=2.64.0"
+      version = ">= 2.64.0"
     }
 
     auth0 = {
       source = "alexkapa/auth0"
-      version = "=0.21.0"
+      version = ">= 0.21.0"
     }
   }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "auth0" {
+
 }
