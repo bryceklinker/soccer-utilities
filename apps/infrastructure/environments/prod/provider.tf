9236784fa3158ea00a1c5bf69e15cfbdb1a9f8de
Bryce Klinker <bryce.klinker@gmail.com>
2021-06-20 07:51:43 -0500

    Fixing provider section of terraform files

--- apps/infrastructure/environments/prod/provider.tf
+++ apps/infrastructure/environments/prod/provider.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hasicorp/azurerm"
-      version = ">= 2.64.0"
+      source = "hashicorp/azurerm"
+      version = "2.64.0"
     }
 
     auth0 = {
-      source = "alexkapa/auth0"
-      version = ">= 0.21.0"
+      source = "alexkappa/auth0"
+      version = "0.21.0"
     }
   }
 
