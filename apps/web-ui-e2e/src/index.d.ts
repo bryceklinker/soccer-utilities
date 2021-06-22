e5e591cfaf727dbef8e4f47c640a44fd595b6807
Bryce Klinker <bryce.klinker@gmail.com>
2021-06-21 22:42:04 -0500

    Updating acceptance test to only check for loading

--- apps/web-ui-e2e/src/index.d.ts
+++ apps/web-ui-e2e/src/index.d.ts
@@ -1,5 +1,5 @@
 declare namespace Cypress {
   interface Chainable<Subject> {
-    login(username: string, password: string): void;
+    login(username?: string, password?: string): void;
   }
 }
--- apps/web-ui-e2e/src/support/app.po.ts
+++ apps/web-ui-e2e/src/support/app.po.ts
@@ -1,1 +1,1 @@
-export const getGreeting = () => cy.get('h1');
+export const getLoading = () => cy.findByLabelText('loading indicator');
