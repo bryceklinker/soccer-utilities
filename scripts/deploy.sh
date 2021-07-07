6cb13800ce469cb5475d58c889046222ab70981c
Bryce Klinker <bryce.klinker@gmail.com>
2021-07-07 09:30:34 -0500

    Attempting to install terraform manually

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,17 @@ set -ex
 
 source "./scripts/common.sh"
 
+export TERRAFORM_VERSION="1.0.1"
 export TARGET_ENVIRONMENT="prod"
 export TARGET_ENVIRONMENT_DIRECTORY="${ENVIRONMENTS_DIRECTORY}/${TARGET_ENVIRONMENT}"
 export PLAN_PATH="${ENVIRONMENTS_DIRECTORY}/${TARGET_ENVIRONMENT}.tfplan"
+
+function install_terraform() {
+  sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+  curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+  sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+  sudo apt-get update && sudo apt-get install terraform="${TERRAFORM_VERSION}"
+}
 
 function extract_rest_api_archive() {
   mkdir -p "${REST_API_OUTPUT_DIRECTORY}"
@@ -42,6 +50,7 @@ function run_end_to_end_tests() {
 }
 
 function main() {
+  install_terraform
   extract_rest_api_archive
   install_npm_packages
   initialize_terraform
