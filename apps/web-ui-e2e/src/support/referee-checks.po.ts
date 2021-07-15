3d994ed98d98b1d7b94424c91a20ca61c823d195
Bryce Klinker <bryce.klinker@gmail.com>
2021-07-14 20:43:29 -0500

    Adding wait for referee checks request

--- apps/web-ui-e2e/src/support/referee-checks.po.ts
+++ apps/web-ui-e2e/src/support/referee-checks.po.ts
@@ -2,13 +2,15 @@ import { CONFIG } from './config';
 
 const navigate = () => {
   const {api} = CONFIG;
-  cy.intercept('GET', `${api.url}/referees/checks`).as('referee-checks');
+  cy.intercept('GET', `${api.url}/referees/checks*`).as('referee-checks');
+
   cy.findByRole('button', { name: 'navigation toggle' }).click();
   cy.findByRole('button', { name: 'referee checks' }).click();
   cy.wait('@referee-checks', {requestTimeout: 60000});
 };
 
 const selectDate = (label: string, day: string) => {
+
   cy.findByRole('button', {name: label}).click();
   cy.findByRole('button', {name: day}).click();
   cy.findByRole('button', {name: 'OK'}).click();
