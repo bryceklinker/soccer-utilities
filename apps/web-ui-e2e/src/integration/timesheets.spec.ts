e8e57e486d1aeaffcb60120c40a0ba511fdb2af7
Bryce Klinker <bryce.klinker@gmail.com>
2021-10-28 16:05:21 -0500

    Adding login to timesheets test

--- apps/web-ui-e2e/src/integration/timesheets.spec.ts
+++ apps/web-ui-e2e/src/integration/timesheets.spec.ts
@@ -2,6 +2,7 @@ import { TimesheetsPage } from '../support/timesheets.po';
 
 describe('Timesheets', () => {
   it('should show all timesheets', () => {
+    cy.login();
     TimesheetsPage.navigate();
 
     TimesheetsPage.findTimesheets().should('have.length.greaterThan', 0);
