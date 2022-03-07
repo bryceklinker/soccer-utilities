3187b0a09e9db83696f49972c256ec15682c10c0
Bryce Klinker <bryce.klinker@gmail.com>
2022-03-06 20:56:53 -0600

    Updating end-to-end tests to use output from terraform for audience

--- infra/environments/prod/outputs.tf
+++ infra/environments/prod/outputs.tf
@@ -2,6 +2,10 @@ output "function_app_url" {
   value = module.api.function_app_url
 }
 
+output "auth0_api_identifier" {
+  value = module.api.api_identifier
+}
+
 output "web_site_url" {
   value = module.site.site_url
 }
