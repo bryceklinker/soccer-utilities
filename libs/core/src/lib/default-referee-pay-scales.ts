6e57117f6ea7c94aa351b37d2d7876f0561ab27a
Bryce Klinker <bryce.klinker@gmail.com>
2022-04-01 23:02:54 -0500

    Updating pay scales

--- libs/core/src/lib/default-referee-pay-scales.ts
+++ libs/core/src/lib/default-referee-pay-scales.ts
@@ -12,27 +12,27 @@ export const DEFAULT_REFEREE_PAY_SCALES: Array<RefereePayScaleModel> = [
   },
   {
     refereeType: RefereeType.Center,
-    amount: 20,
+    amount: 25,
     ageGroup: { age: 10, gender: Gender.Unknown },
   },
   {
     refereeType: RefereeType.Center,
-    amount: 30,
+    amount: 35,
     ageGroup: { age: 12, gender: Gender.Unknown },
   },
   {
     refereeType: RefereeType.Center,
-    amount: 40,
+    amount: 45,
     ageGroup: { age: 14, gender: Gender.Unknown },
   },
   {
     refereeType: RefereeType.Center,
-    amount: 45,
+    amount: 55,
     ageGroup: { age: 16, gender: Gender.Unknown },
   },
   {
     refereeType: RefereeType.Center,
-    amount: 50,
+    amount: 65,
     ageGroup: { age: 19, gender: Gender.Unknown },
   },
 
@@ -48,17 +48,17 @@ export const DEFAULT_REFEREE_PAY_SCALES: Array<RefereePayScaleModel> = [
   },
   {
     refereeType: RefereeType.Assistant,
-    amount: 25,
+    amount: 30,
     ageGroup: { age: 14, gender: Gender.Unknown },
   },
   {
     refereeType: RefereeType.Assistant,
-    amount: 30,
+    amount: 35,
     ageGroup: { age: 16, gender: Gender.Unknown },
   },
   {
     refereeType: RefereeType.Assistant,
-    amount: 35,
+    amount: 40,
     ageGroup: { age: 19, gender: Gender.Unknown },
   },
 ];
